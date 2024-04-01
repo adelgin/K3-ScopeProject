ab178ea475124c5fb96216303d88c1e08247a422
Adel <leto.456@mail.ru>
2024-04-01 22:28:44 +0300

    init commit

---
+++ src/ScopeReader.kt
@@ -0,0 +1,22 @@
+import java.util.*
+
+class ScopeReader(private val ScopeStr : String) {
+    private val openScopeMap = mapOf('{' to '}', '(' to ')', '[' to ']', '<' to '>')
+    private var openScopeStack = Stack<Char>()
+    public fun reader() : Boolean {
+        for (i in ScopeStr) {
+            if (openScopeMap.containsKey(i)) {
+                openScopeStack.push(i)
+            }
+            if (openScopeMap.containsValue(i)) {
+                if (i == openScopeMap[openScopeStack.peek()]) {
+                    openScopeStack.pop()
+                }
+            }
+        }
+        if (openScopeStack.isEmpty()) {
+            return true
+        }
+        return false
+    }
+}
